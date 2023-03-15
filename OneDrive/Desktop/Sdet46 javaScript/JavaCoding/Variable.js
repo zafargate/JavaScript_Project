469b7916b7c73717ead0481c5b73389e95f9e58d
zafar alam <113663507+zafargate@users.noreply.github.com>
2023-03-15 15:52:47 +0530

    variable

---
+++ OneDrive/Desktop/Sdet46 javaScript/JavaCoding/Variable.js
@@ -0,0 +1,15 @@
+
+//1. Var Data Type
+
+// var a=10;
+// var a=20;
+// var a='javaCSript'
+
+//2. Let Variable
+//   let a=10;
+//     a=20;
+
+//3.const Variable
+   const a  =30;
+
+console.log(a);
